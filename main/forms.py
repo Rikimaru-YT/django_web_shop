7a7cfee36e8fa59b3b0aa315ec9c8290134d5542
HP <vtyo394@gmail.com>
2026-09-24 00:00:29 +0500

    обновление проекта

--- main/forms.py
+++ main/forms.py
@@ -8,19 +8,24 @@ class ContactMessageForm(forms.ModelForm):
         fields = ['name', 'email', 'number', 'subject', 'message']
         widgets = {
             'name': forms.TextInput(attrs={
-                'class': 'form-control ps-3 me-3'
+                'class': 'form-control ps-3 me-3',
+                'placeholder': 'Write your name here'
             }),
             'email': forms.EmailInput(attrs={
-                'class': 'form-control ps-3'
+                'class': 'form-control ps-3',
+                'placeholder': 'Write your email here'
             }),
             'number': forms.TextInput(attrs={
-                'class': 'form-control ps-3'
+                'class': 'form-control ps-3',
+                'placeholder': 'Write your phone number here'
             }),
             'subject': forms.TextInput(attrs={
-                'class': 'form-control ps-3'
+                'class': 'form-control ps-3',
+                'placeholder': 'Write your subject here'
             }),
             'message': forms.Textarea(attrs={
-                'class': 'form-control ps-3'
+                'class': 'form-control ps-3',
+                'placeholder': 'Write your message here'
             })
         }
 
